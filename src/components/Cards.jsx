66b18b9f41f6558f3a3b318cf985ebf403657e2f
tonymj76 <demeyortj@gmail.com>
2021-01-28 14:50:23 +0100

    card items

---
+++ src/components/Cards.jsx
@@ -0,0 +1,19 @@
+
+import Notification from "../assets/icons/svg/notification.svg";
+
+export default function Cards({num, item}) {
+  
+  
+  return (
+    <div className="card flex-item">
+      <div className="card-info">
+        <p className="card-number">{num}</p>
+        <p className="card-text">{item}</p>
+      </div>
+
+      <div className="card-icon">
+        <img src={Notification} alt="Notification" />
+      </div>
+    </div>
+  );
+}
