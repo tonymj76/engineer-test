f8fbc23ddf0baf0178b3cf178e8fa6b7c8142ffc
tonymj76 <demeyortj@gmail.com>
2021-01-28 16:06:31 +0100

    hot fix

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -8,6 +8,7 @@ import pin from "../assets/icons/svg/pin.svg";
 import phone from "../assets/icons/svg/phone.svg";
 import notebook from "../assets/icons/svg/notebook.svg";
 import envelope from "../assets/icons/svg/envelope.svg";
+import elipse from "../assets/icons/svg/elipse.svg";
 import n from "../assets/icons/svg/n.svg";
 import cls from "classnames";
 
@@ -65,6 +66,23 @@ function App() {
     ],
     []
   );
+  const userTimeline = useMemo(
+    () => [
+      {
+        item: "",
+        subText: "22:10 15/09/2020 <span>.</span> Web ",
+      },
+      {
+        item: "",
+        subText: "",
+      },
+      {
+        item: "Searched “Journal Entries” on Division module",
+        subText: "22:10 15/09/2020 <span>.</span> Web",
+      },
+    ],
+    []
+  );
   return (
     <div>
       <TopNavBar />
@@ -92,6 +110,33 @@ function App() {
                 </li>
               ))}
             </ul>
+          </CardUserFlow>
+
+          <div className="table"></div>
+
+          <CardUserFlow headIcon={iconSpeaker} headText="Module History">
+            <div id="timeline-content">
+              <ul className="timeline">
+                <li className="event">
+                  <h3>Searched “Journal Entries” on Division module</h3>
+                  <p>
+                    22:10 15/09/2020 <span><img src={elipse} alt="elipse"/></span> Web
+                  </p>
+                </li>
+                <li className="event">
+                  <h3>Searched “Fingerprint record” on Division module</h3>
+                  <p>
+                    22:10 15/09/2020 <span><img src={elipse} alt="elipse"/></span> Web
+                  </p>
+                </li>
+                <li className="event">
+                  <h3>Searched “Journal Entries” on Division module</h3>
+                  <p>
+                    22:10 15/09/2020 <span><img src={elipse} alt="elipse"/></span> Web
+                  </p>
+                </li>
+              </ul>
+            </div>
           </CardUserFlow>
         </div>
       </DashboardBody>
