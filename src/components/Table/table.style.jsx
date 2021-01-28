466205f2f1bf4d2c182c4bcb39b1a133d2f82bea
tonymj76 <demeyortj@gmail.com>
2021-01-28 19:11:10 +0100

    hot fix

--- src/components/Table/table.style.jsx
+++ src/components/Table/table.style.jsx
@@ -5,9 +5,11 @@ export const TableContainer = styled.div`
   background: #ffffff;
   box-shadow: 0px 4px 30px rgba(192, 192, 192, 0.25);
   border-radius: 6px;
+  flex-grow: 3;
   table {
     width: 100%;
     text-align: left;
+    
     border-spacing: 0px;
     thead{
        background-color:#F3F6F9;
